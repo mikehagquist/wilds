b78b929b5c897ceee6f8e89dd735b264e78e156e
mikehagquist <mikehagquist@gmail.com>
2018-09-12 15:03:09 -0400

    Cleaned up a bit.

--- wilds/urls.py
+++ wilds/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('animals/', include('animals.urls')),
     path('admin/', admin.site.urls),
+    path('', include('animals.urls')),
 ]
