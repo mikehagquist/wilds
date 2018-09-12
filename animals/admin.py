dc4af2ac6315c560d33ef0aa983f0e6d8c1b6785
mikehagquist <mikehagquist@gmail.com>
2018-09-12 13:44:11 -0400

    Refactored model.  Changed data relationships.

--- animals/admin.py
+++ animals/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Animal)
+admin.site.register(Phylum)
+admin.site.register(ClassA)
+admin.site.register(Order)
+admin.site.register(Family)
+admin.site.register(Genus)
+admin.site.register(Species)
--- animals/models.py
+++ animals/models.py
@@ -11,29 +11,76 @@ from django.db import models
 #     choice_text = models.CharField(max_length=200)
 #     votes = models.IntegerField(default=0)
 
-class Animal(models.Model):
-    animal_name = models.CharField(max_length=200)
-
 
 class Phylum(models.Model):
-    phylum_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    phylum_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.phylum_name
+
+    class Meta:
+        verbose_name_plural = "1. Phylum"
 
 
-class Class(models.Model):
-    class_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+class ClassA(models.Model):
+    class_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.class_name
+
+    class Meta:
+        verbose_name_plural = "2. Class"
+        verbose_name = "class"
 
 
 class Order(models.Model):
-    order_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    order_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.order_name
+
+    class Meta:
+        verbose_name_plural = "3. Order"
 
 
 class Family(models.Model):
-    family_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    family_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.family_name
+
+    class Meta:
+        verbose_name_plural = "4. Family"
 
 
 class Genus(models.Model):
-    genus_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    genus_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.genus_name
+
+    class Meta:
+        verbose_name_plural = "5. Genus"
 
 
 class Species(models.Model):
-    species_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    species_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.species_name
+
+    class Meta:
+        verbose_name_plural = "6. Species"
+
+
+class Animal(models.Model):
+    animal_name = models.CharField(max_length=200)
+    phylum = models.ForeignKey(Phylum, on_delete=models.CASCADE)
+    classa = models.ForeignKey(ClassA, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    family = models.ForeignKey(Family, on_delete=models.CASCADE)
+    genus = models.ForeignKey(Genus, on_delete=models.CASCADE)
+    species = models.ForeignKey(Species, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.animal_name
