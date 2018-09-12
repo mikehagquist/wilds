912aba5e512b862b238788b5c5c783ec39dd093d
mikehagquist <mikehagquist@gmail.com>
2018-09-12 12:04:14 -0400

    Added data model.

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+#
+#
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+
+class Animal(models.Model):
+    animal_name = models.CharField(max_length=200)
+
+
+class Phylum(models.Model):
+    phylum_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+
+class Class(models.Model):
+    class_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    order_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+
+class Family(models.Model):
+    family_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+
+class Genus(models.Model):
+    genus_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+
+class Species(models.Model):
+    species_name = models.ForeignKey(Animal, on_delete=models.CASCADE)
